bca202328de012321d0dca65f61a99746263660a
Sohel <jmsohelrana04@gmail.com>
2022-03-31 17:46:53 +0600

    review component added some css style

--- src/components/reviewItems/ReviewItem.js
+++ src/components/reviewItems/ReviewItem.js
@@ -1,10 +1,37 @@
 import React from "react";
+import "./ReviewItem.css";
 
 const ReviewItem = ({ product }) => {
-  const { name } = product;
+  const { name, img, price, shipping, quantity } = product;
   return (
-    <div>
-      <h1> {name}</h1>
+    <div className="review-item">
+      <div>
+        <img src={img} alt="No pic" />
+      </div>
+      <div className="review-item-details-container">
+        <div className="review-item-details">
+          <p className="product-name" title={name}>
+            {name.length > 20 ? name.slice(0, 20) + "..." : name}
+          </p>
+          <p>
+            Price: <span className="orange-color">${price}</span>
+          </p>
+          <p>
+            <small>
+              Shipping charge:{" "}
+              <span className="orange-color">
+                <b>${shipping}</b>
+              </span>
+            </small>
+          </p>
+          <p>
+            <small>Quantity: {quantity}</small>
+          </p>
+        </div>
+        <div className="delete-container">
+          <button>delete</button>
+        </div>
+      </div>
     </div>
   );
 };
